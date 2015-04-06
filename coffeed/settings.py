45c34165fa53a6e44d01c0b334ee0aabd22e855a
Brytny <Brytny@Brytnys-iMac.local>
2015-04-05 21:40:33 -0500

    preparing for heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,7 +83,15 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -104,11 +113,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
- os.path.join(BASE_DIR, 'Templates'),
+ os.path.join(MAIN_DIR, 'Templates'),
  )
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
+ os.path.join(MAIN_DIR, 'static'),
  )
 
 STATIC_ROOT = 'staticfiles'
